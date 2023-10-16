773d56fab12eee9a33a10220afb0b58a7ded2115
DELHI BABU <rldb143@gmail.com>
2023-10-16 17:52:07 +0530

    js done

--- app.js
+++ app.js
@@ -3,43 +3,89 @@ let btn = document.getElementById("btn");
 let page = 1;
 let showmore = document.getElementById("showmorebtn");
 let section = document.querySelector("section");
+let footerdiv=document.getElementById("footer-path")
+let footer=document.querySelector('footer')
 
 async function getmoviesdata() {
   try{
+    showmore.disabled = true
     let searchinput = document.getElementById("searchinput").value;
 
   if (searchinput.trim() === "") {
-    showmore.style.display="none"
+    // showmore.style.display="none"
+    showmore.disabled = false;
     alert("The Field Cannot Be Empty")
-    return;
+    return
   }
+
+
+
   let data = await fetch(`http://www.omdbapi.com/?page=${page}&s=${searchinput}&apikey=77908198`);
   data = await data.json();
   console.log(data);
 
-  data.Search.map(({ Title, Poster, Year}) => {
+  if (data.Error === 'Movie not found!')  {
+    showmore.style.display = "none";
+    // footer.style.display="none"
+    showmore.disabled = false;
+  section.innerHTML=`<img src="./pagenotfound.gif" width=300px height=400px style="margin-bottom: 90px;"  alt="" /> `
+    // alert("search results not found")
+    
+    return 
+  }
+
+  if (!data.Search) {
+    console.log("No search results found.");
+    showmore.disabled = false;
+    return;
+  }
+
+  // if (data.Poster === "N/A") {
+  //   section.innerHTML=`<img src=${error2.png}/>`
+  // }
+
+  data.Search.forEach(({ Title, Poster, Year}) => {
+    let imgSrc = Poster !== "N/A" ? Poster : "./na.gif";
     section.innerHTML += `
       <div id="main-div">
         <div id="second-div">
-          <img src=${Poster}>
+        <img src=${imgSrc}>
           <h3>${Title}</h3>
           <h4>Year : ${Year}</h4>
         </div>
       </div>
     `;
+
   });
 
-  page++;
+
+  
   if (data.totalResults <= 10) {
     showmore.style.display = "none";
-  } else {
+    // footer.style.display="block"
+  } 
+  else  {
     showmore.style.display = "block";
+    // footer.style.display ="block"
   }
+
+
+  showmore.disabled = false
+  if (page === 0) {
+    footer.style.display = "none";
+  } else {
+    footer.style.display = "block";
+  } 
+
+  page++;
+
   }
   catch(err)
   {
+    
     console.log(err);
-    alert("Movie Not Found")
+    showmore.disabled = false;
+    // alert("Movie Not Found")
   }
 }
 
@@ -48,35 +94,10 @@ searchbox.addEventListener("submit", (event) => {
   page = 1;
   section.innerHTML = "";
   getmoviesdata();
+  // footer.style.display="block"
 });
-
-// searchinput.addEventListener("input", () => {
-//     // When the input value changes, trigger the search
-//     page = 1;
-//     getmoviesdata();
-//   });
-
 showmore.addEventListener('click', () => {
   getmoviesdata();
 });
 
-
-
-// //^ IMAGE SLIDER
-
-// let image=document.getElementById("image")
-// let bi=document.querySelectorAll(".bi")
-
-// let imageList = ["https://unsplash.com/@frostroomhead","2.jpeg","3.jpeg","4.jpeg","5.jpeg","6.jpeg"]
-
-// let count=0;
-// // image.src=imageList[index]
-
-// function displaycolors(){
-  
-//      main.style.backgroundColor=imageList[count]
-//      count=(count+1 ) % colors.length
-// }
-// displaycolors();
-// setInterval(displaycolors,2000)
 
